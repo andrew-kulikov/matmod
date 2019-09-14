eea8e23bda51618dd9aee121a0c0d725d352b34a
Andrew <andrew.1.kulikov@gmail.com>
2019-09-14 21:57:10 +0300

    Add point and interval estimations

--- src/statlib/simple_distribs.py
+++ src/statlib/simple_distribs.py
@@ -35,15 +35,15 @@ class Logistic:
 
 
 class Cauchy: # needed 3
-    def __init__(self):
-        self.uniform = Uniform()
+    def __init__(self, generator):
+        self.uniform = Uniform(generator)
 
     def next(self, x0: float, gamma: float):
         x, y = 0, 0
         while True:
             x = self.uniform.next(-1, 1)
             y = self.uniform.next(-1, 1)
-            if x ** 2 + y ** 2 < 1.0 or y == 0.0:
+            if x ** 2 + y ** 2 <= 1.0 or y == 0.0:
                 break
 
         return x0 + gamma * x / y
